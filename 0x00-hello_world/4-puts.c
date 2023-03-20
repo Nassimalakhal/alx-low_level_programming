a0b363954e8d115ff133341a283d3e4ace840166
Nassima <nassima.lakhal16@gmail.com>
2023-03-20 05:56:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,14 @@
 
 
 /**
-* main - A Programming is like building a multilingual
-puzzle, followed by a new line.
-* Return: 0 (success)
+* main - Programming is like building a multilingual
+puzzle
+* Return: 0 (Success)
 */
-
-
 int main(void)
-
 {
-  puts("\"Programming is like building a multilingual puzzle"); 
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 

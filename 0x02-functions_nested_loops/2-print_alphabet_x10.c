996ede2991a377b927079899cd87a0de8df3c06d
Nassima <nassima.lakhal16@gmail.com>
2023-03-21 15:07:39 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Entry 10 times alphabet
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+for (c = '1', c <=10, c++)
+ {
+  print("abcdefghijklmnopqrstuvwxyz");
+  print("\n");
+ }
+}
+

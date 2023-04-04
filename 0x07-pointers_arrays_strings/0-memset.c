05ee5298fd3484875b7d449ceab1c7ec763899f5
Nassima <nassima.lakhal16@gmail.com>
2023-04-03 17:05:36 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,9 +9,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
   int i = 0;
-  for(i =0; n > 0; i++){
-  s[i] = b;
-  n--;
+  for(i =0; n > 0; i++)
+  {
+   s[i] = b;
+   n--;
   }
   return (s);
 }

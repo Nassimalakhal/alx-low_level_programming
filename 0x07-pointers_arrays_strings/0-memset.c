582eaf675dbf1681fa477ddc30fbb0fadea19064
Nassima <nassima.lakhal16@gmail.com>
2023-04-03 17:02:58 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,12 +4,12 @@
 * @s: memory area
 * @b: the constant byte
 * @n: the first  bytes of the memory to be changed
-* Return: number of bytes to be changed
+* Return: a pointer to the memory area s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
   int i = 0;
-  for(; n > 0; i++){
+  for(i =0; n > 0; i++){
   s[i] = b;
   n--;
   }

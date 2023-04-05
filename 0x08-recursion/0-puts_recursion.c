bd50e2e79cbbb7174fae79bfc908e718c789c133
Nassima <nassima.lakhal16@gmail.com>
2023-04-05 16:53:13 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string followed by a new line
+* @s: input
+* Return: always 0 (success)
+*/
+void _puts_recursion(char *s)
+{
+  _putchar(*s);
+  _putchar("\n");
+ return (0);
+}

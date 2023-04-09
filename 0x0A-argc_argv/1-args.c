13d4b95f904b6ce0c0b2e62a287ba6c3daeaa74a
Nassima <nassima.lakhal16@gmail.com>
2023-04-08 20:43:40 -0700

    The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,20 +5,9 @@
 * @argv: array of strings
 * Return: always 0.
 */
-int main (int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-int count;
-printf ("hello \"%s\".\n",argv[0]);
-if (argc > 1)
-{
-for (count = 1; count < argc; count++)
-{
-printf("argv[%d] = %s\n", count, argv[count]);
-}
-}
-else
-{
-printf("hello, world.\n");
-}
-return 0;
+	printf("%d\n", argc + 1);
+
+	return (0);
 }

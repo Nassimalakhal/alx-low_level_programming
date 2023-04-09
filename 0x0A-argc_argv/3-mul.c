2bc2f0f5b8b9180a41edf5c377fb1d8581f2ac1f
Nassima <nassima.lakhal16@gmail.com>
2023-04-08 21:51:17 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for my program
+ * @argc: This is the number of arguments
+ * @argv: This is the array of arguments
+ *
+ * Return: Zero upon program success
+ */
+
+int main(int argc, char *argv[])
+{
+        int i;
+
+        int result = 1;
+
+        for (i = 1; i < argc; i++)
+        {
+                result = result * argv[i];
+        }
+        printf("%d\n", result);
+        return (0);
+}

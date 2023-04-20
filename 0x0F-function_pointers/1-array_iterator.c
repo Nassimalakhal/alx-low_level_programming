fcec27ff4c36d1bf132b9eb4dbf6d3b3de76e23f
Nassima <nassima.lakhal16@gmail.com>
2023-04-19 19:26:23 -0700

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 /**
-* array_iterator - function that executes a function given as a parameter on each element of an array
+* array_iterator - function that executes a function given as a parameter
 * @array: array
 * @size: how many elem to print
 * @action: pointer to print in regular or hex
